4caf1012f61e9a13f60bf6bf4af4f5ffef98c553
Douglas <douglas.m.f.1995@gmail.com>
2026-03-16 12:26:30 -0300

    Adicionando o favicon

--- frontEnd/src/services/http.ts
+++ frontEnd/src/services/http.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export const http = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api',
+    baseURL: 'https://lista-compras-fullstack-laravel-react.onrender.com/api',
     headers: {
         'Content-Type': 'application/json'
     }
